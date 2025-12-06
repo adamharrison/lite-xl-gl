de56224109a643930593c7e8ed15b6f5a917b2d8
Adam Harrison <adamdharrison@gmail.com>
2025-12-06 14:08:26 -0500

    Allowed for cross-compiling.

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 ## gl
 
 This plugin implements an SDL-based hardware accelerated renderer.
+
+### Building
+
+This plugins statically compiles SDL with GPU features into the plugin.
+
+#### Linux
+
+To build, on linux, simply do:
+
+```
+build.sh
+```
+
+
+#### Linux -> Windows
+
+```
+./build.sh clean && CC=x86_64-w64-mingw32-gcc AR=x86_64-w64-mingw32-gcc-ar WINDRES=x86_64-w64-mingw32-windres \
+  CMAKE_DEFAULT_FLAGS="-DCMAKE_FIND_ROOT_PATH_MODE_PROGRAM=NEVER -DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY=NEVER -DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=NEVER\
+  -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_SYSTEM_INCLUDE_PATH=/usr/share/mingw-w64/include"\
+  GIT2_CONFIGURE="-DDLLTOOL=x86_64-w64-mingw32-dlltool" BIN=libgl.dll ./build.sh -lgdi32 -DLIBGL_VERSION='"'$VERSION-x86_64-windows-`git rev-parse --short HEAD`'"'
+```
+
+
--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 : ${CC=gcc}
 : ${BIN=libgl.so}
 : ${MAKE=make}
+: ${AR=ar}
 
 
 COMPILE_FLAGS="$CFLAGS -I`pwd`/lib/lite-xl/resources/include -I`pwd`/lib/prefix/include -fPIC -Ilib/lite-xl/resources/include" # We specifically rename this and LDFLAGS, because exotic build environments export these to subprocesses.
@@ -25,7 +26,9 @@ if [[ "$@" != *"-lfreetype"* ]]; then
   LLSRCS=" $LLSRCS lib/freetype/src/base/ftsystem.c lib/freetype/src/base/ftinit.c lib/freetype/src/base/ftdebug.c lib/freetype/src/base/ftbase.c \
     lib/freetype/src/base/ftbbox.c lib/freetype/src/base/ftglyph.c lib/freetype/src/sfnt/sfnt.c lib/freetype/src/truetype/truetype.c lib/freetype/src/raster/raster.c \
     lib/freetype/src/smooth/smooth.c lib/freetype/src/autofit/autofit.c lib/freetype/src/psnames/psnames.c lib/freetype/src/pshinter/pshinter.c lib/freetype/src/cff/cff.c \
-    lib/freetype/src/gzip/ftgzip.c lib/freetype/src/base/ftbitmap.c"
+    lib/freetype/src/gzip/ftgzip.c lib/freetype/src/base/ftbitmap.c lib/freetype/src/base/ftmm.c"
+  [ ! -e "lib/prefix/lib/libfreetype2.a" ] && { $CC -c $LINK_FLAGS $COMPILE_FLAGS $LLSRCS -c && $AR rc lib/prefix/lib/libfreetype2.a *.o && rm *.o || exit -1; }
+  LINK_FLAGS="$LINK_FLAGS -lfreetype2"
 fi
 
 
@@ -35,7 +38,7 @@ if [[ "$@" != *"-lSDL"* ]]; then
   [ ! -e "lib/SDL/build" ] && { cd lib/SDL && mkdir build && cd build && cmake .. -G "Unix Makefiles" $CMAKE_DEFAULT_FLAGS \
     -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DSDL_INSTALL=ON -DSDL_INSTALL_DOCS=OFF -DSDL_DEPS_SHARED=ON \
 		-DSDL_AVX=OFF -DSDL_AVX2=OFF -DSDL_AVX512F=OFF -DSDL_SSE3=OFF -DSDL_SSE4_1=OFF -DSDL_SSE4_2=OFF \
-		-DSDL_DBUS=ON -DSDL_IBUS=ON -DSDL_AUDIO=OFF -DSDL_GPU=ON -DSDL_RPATH=OFF -DSDL_PIPEWIRE=OFF \
+		-DSDL_DBUS=OFF -DSDL_IBUS=OFF -DSDL_AUDIO=OFF -DSDL_GPU=ON -DSDL_RPATH=OFF -DSDL_PIPEWIRE=OFF \
 		-DSDL_CAMERA=OFF -DSDL_JOYSTICK=OFF -DSDL_HAPTIC=OFF -DSDL_HIDAPI=OFF -DSDL_DIALOG=OFF \
 		-DSDL_POWER=OFF -DSDL_SENSOR=OFF -DSDL_VULKAN=OFF -DSDL_LIBUDEV=OFF -DSDL_SHARED=OFF -DSDL_STATIC=ON \
 		-DSDL_X11=ON -DSDL_WAYLAND=ON -DSDL_VIDEO=ON -DSDL_TESTS=OFF -DSDL_EXAMPLES=OFF -DSDL_VENDOR_INFO=lite-xl && 
@@ -43,4 +46,5 @@ if [[ "$@" != *"-lSDL"* ]]; then
   LINK_FLAGS="-lSDL3 $LINK_FLAGS"
 fi
 
-$CC $COMPILE_FLAGS *.c api/*.c $@ -shared -o $BIN $LINK_FLAGS 
+[[ $OSTYPE == 'msys'* || $OSTYPE == 'cygwin'* || $CC == *'mingw'* ]] && LINK_FLAGS="$LINK_FLAGS  -lgdi32 -limm32 -lsetupapi -lwinmm -lole32 -lversion -luuid"
+$CC $COMPILE_FLAGS *.c api/*.c $@ -shared -o $BIN $LINK_FLAGS
